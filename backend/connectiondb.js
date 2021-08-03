261f26d9c3b5be5cfaf7ad370b1473764b507e41
Descamps <lucasdescm@gmai.com>
2021-08-03 17:58:02 +0200

    initial backend commit
    Creation de la connection node a mysql
    Creation de la base de donnée et des differentes tables

---
+++ backend/connectiondb.js
@@ -0,0 +1,22 @@
+var mysql = require('mysql');
+var dotenv = require('dotenv').config({path: './.env'});
+
+const DB_USER = process.env.DB_USER;
+const DB_HOST = process.env.DB_HOST;
+const DB_PASS = process.env.DB_PASS;
+
+const db = mysql.createConnection({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASS,
+    database: 'groupomania'
+});
+
+db.connect((err) => {
+    if(err) {
+        throw err;
+    }
+    console.log('Connecté à la base de donnée groupomania');
+});
+
+module.exports = db;
